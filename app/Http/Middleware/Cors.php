7fcd7f378f51f88a627ce61b687fe4a6285d2de6
Shehabuddin-Tushar <freelancerrtushar@gmail.com>
2024-01-30 12:40:51 +0600

    some changes

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -16,9 +16,8 @@ class Cors
     public function handle(Request $request, Closure $next)
     {
         $response = $next($request);
-
-        $response->headers->set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
         $response->headers->set('Access-Control-Allow-Origin', '*');
+        $response->headers->set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
         $response->headers->set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
         $response->headers->set('Access-Control-Allow-Headers', 'Content-Type, X-Auth-Token, Origin, Authorization');
 
